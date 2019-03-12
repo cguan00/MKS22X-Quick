ed69027ef377fa34c6c4011ef62c2d123202ebf7
Christy Guan <cguan00@stuy.edu>
2019-03-12 14:46:34 -0400

    testing partition

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+  public static void main(String[] args){
+    int[] data1 = {10, 80, 30, 90, 40, 50, 70};
+    System.out.println(Quick.printArray(data1));
+    System.out.println(Quick.partition(data1, 0, 6));
+    System.out.println(Quick.printArray(data1));
+  }
+}
