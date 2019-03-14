3373819ddfe71b6a8ac9216d5e9702e8d5a61e17
Christy Guan <cguan00@stuy.edu>
2019-03-13 22:16:58 -0400

    testing methods

--- Driver.java
+++ Driver.java
@@ -2,8 +2,20 @@ public class Driver{
   public static void main(String[] args){
     int[] data1 = {10, 80, 30, 90, 40, 50, 70};
     System.out.println(Quick.printArray(data1));
-    System.out.println(Quick.partition(data1, 0, 6));
+    // System.out.println(Quick.partition(data1, 0, 6));
+    // System.out.println(Quick.printArray(data1));
+
+    //testing quickselect
+    System.out.println(Quick.quickselect(data1, 0));//10
+    System.out.println(Quick.quickselect(data1, 1));//30
+    System.out.println(Quick.quickselect(data1, 2));//40
+    System.out.println(Quick.quickselect(data1, 3));//50
+    System.out.println(Quick.quickselect(data1, 4));//70
+    System.out.println(Quick.quickselect(data1, 5));//80
+    System.out.println(Quick.quickselect(data1, 6));//90
+
+    //try quick sort
+    Quick.quicksort(data1);
     System.out.println(Quick.printArray(data1));
-    System.out.println(Quick.checkPartition(data1));
   }
 }
